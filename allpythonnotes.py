2d7d6d0614acd7c5cc0ff5788207f248a3aab9a2
stokelycw <50683593+stokelycw@users.noreply.github.com>
2019-05-30 18:29:28 -0700

    Create allpythonnotes.py

---
+++ allpythonnotes.py
@@ -0,0 +1,30 @@
+# Terms and Ideas
+#
+#   Function: A block of code that only runs when it is called
+#   Recursion: When a function calls itself
+#   CLASS: A template for creating objects.
+#   Object: An encapsulation of variables and functions into a single entity
+#       Get their variables and functions from classes
+#   TRINARY OPERATIONS/CONDITIONALS
+#   Algorithm: A way of solving a math/logic problem
+#   Concatenation: gluing a cat's nose to another's tail
+#   Loop: The code will run repeatedly
+#       While Loops repeat while a statement is true (_)
+#       For Loops repeat for members in a sequence (.)
+#   IF: Gives a condition
+#   ELIF: If previous conditions are false, try this one next.
+#   ELSE: If all conditions are false, execute the following.
+#   ==: Used in conditions; says two ints are equal; the same object
+#   IS: Used in conditions; says two ints have the save numerical value
+#   INPUT: Takes input from user interface
+# Syntax
+#   input("prompt")
+#   else: enter, page break
+#   elif: "#condition"
+#   if: "#condition"
+#   for x in range(start, stop, step)
+#   for x in range(start, stop)
+#   for x in range(stop)
+#       task
+#   while x < 100
+#       x must be defined above and incremented, and while must be followed by a task

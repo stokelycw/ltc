36f3880750a3a0241c6947ff95aac841e824713c
stokelycw <50683593+stokelycw@users.noreply.github.com>
2019-05-30 18:31:05 -0700

    Create bdgcal.py

---
+++ bdgcal.py
@@ -0,0 +1,25 @@
+# Budget Calendar
+# To calculate how much money is available to spend daily to meet savings goals
+# Variables:
+#   Overall goal
+#   Daily goal
+#   Days until End
+#   Balance
+#   Daily Income
+#   Deductions
+#   Spending Limit
+# Deductions Class
+#   Bills
+#   Money spent
+
+#  Process: Balance = Balance + Daily income - Deductions
+#   Daily goal = Overall goal / Days until End
+#   Spending Limit = Balance - Daily goal
+
+# Get inputs from file (???)
+import csv
+with open() as csvDataFile:
+    csvReader = csv.Reader(csvDataFile)
+    for column in csvReader:
+        print(column)
+    
